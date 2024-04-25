a82557e2ba2338c9f3587682fbfb7774e848e411
cmbarros <carlosmbarros@icloud.com>
2024-04-24 17:24:40 -0700

    Initial Commit

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Carlos Barros on 4/24/24.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
